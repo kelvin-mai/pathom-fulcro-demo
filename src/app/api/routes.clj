f5d23e88106192efbdb2da503e3ad41aed2fbf5f
Kelvin Mai <kelvin.mai002@gmail.com>
2021-06-07 20:16:05 -0700

    set up pathom

--- src/app/api/routes.clj
+++ src/app/api/routes.clj
@@ -1,15 +1,37 @@
 (ns app.api.routes
-  (:require
-   [reitit.ring :as ring]))
+  (:require [clojure.core.async :as async]
+            [reitit.ring :as ring]
+            [com.fulcrologic.fulcro.server.api-middleware :as fulcro-mw]
+            [taoensso.timbre :as log]
+            [com.wsscode.pathom.core :as p]
+            [com.wsscode.pathom.connect :as pc]))
+
+(def my-resolvers [])
+
+(def parser
+  (p/parallel-parser
+   {::p/env {::p/reader                [p/map-reader
+                                        pc/parallel-reader
+                                        pc/open-ident-reader]
+             ::pc/mutation-join-global [:tempids]}
+    ::p/mutate pc/mutate-async
+    ::p/plugins [(pc/connect-plugin {::pc/register my-resolvers})
+                 (p/post-process-parser-plugin p/elide-not-found)
+                 p/error-handler-plugin]}))
 
 (def ping-routes
-  ["/health_check" {:name ::ping
+  ["/health" {:name ::ping
                     :get (fn [_]
+                           (log/info "Route" ::ping)
                            {:status 200
                             :body {:ping "pong"}})}])
 
 (def route-config
-  {:data {:middleware []}})
+  {:data {:middleware [[fulcro-mw/wrap-api {:uri "/api"
+                                            :parser (fn [q]
+                                                      (async/<!! (parser {} q)))}]
+                       fulcro-mw/wrap-transit-params
+                       fulcro-mw/wrap-transit-response]}})
 
 (defn routes []
   (ring/ring-handler
