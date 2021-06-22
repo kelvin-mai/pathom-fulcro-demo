9ffb9ae85c5d5e601b606c07b748ca95125a8dc0
Kelvin Mai <kelvin.mai002@gmail.com>
2021-06-21 18:16:18 -0700

    clj -X:dev starts dev mode

--- dev/user.clj
+++ dev/user.clj
@@ -2,11 +2,25 @@
   (:require [integrant.core :as ig]
             [integrant.repl :as ig-repl :refer [go halt reset reset-all]]
             [integrant.repl.state :as state]
+            [nrepl.server]
+            [cider.nrepl :refer [cider-nrepl-handler]]
             [app.system :as system]))
 
 (ig-repl/set-prep!
  (fn []
    (system/read-config-file "resources/config.edn")))
+
+(defn start-dev 
+  "
+  Entrypoint for clj -X:dev
+  Start nrepl server with cider middleware and start system
+  WARNING: Do not reload user namespace
+  "
+  [& _]
+  (nrepl.server/start-server :port 12345
+                             :bind "0.0.0.0"
+                             :handler cider-nrepl-handler)
+  (go))
 
 (comment
   (go)
